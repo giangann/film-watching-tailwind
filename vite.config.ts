61ad61b1825fb76ca3af06f710edf1928653e126
giangann <70595717+giangann@users.noreply.github.com>
2023-05-01 00:09:29 +0700

    tailwind CLI - fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import react from '@vitejs/plugin-react';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import css from 'rollup-plugin-css-only'; // Import the module here
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh(), css({ output: 'output.css' })],
 });
